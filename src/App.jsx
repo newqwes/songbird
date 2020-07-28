edfa4e2a4fb379443496a6d1bfbe22d9818c7ab9
Nazar Ashyrov <newnewqwes@gmail.com>
2020-07-29 00:57:59 +0300

    refactor: change structure, add style module, sass

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className={styles.title}>Добрый вечер</h1>
+    </div>
+  );
+}
+
+export default App;
