acf1ffd325cdd9e8abb0c5e296d75e4e1ff80789
Nazar Ashirov <newnewqwes@gmail.com>
2020-07-30 23:35:27 +0300

    feat: add state

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,27 @@ import AnswerContainer from './components/Answer/AnswerContainer';
 import QuestionContainer from './components/Question/QuestionContainer';
 
 const App: React.FC = () => {
+
+  let warmUp = {
+      global: 'Разминка',
+      cn: {
+        id: 0,
+        languageName: 'Китайский',
+        audio: '../../assets/saundLanguage/china.mp3',
+        flag: '../../assets/img/defFlag.jpg',
+        titleDesc: 'Путунхуа (общепринятое название)',
+        textDesc: 'Общепринятый нормативный китайский, или общенациональный язык путунхуа, основан на диалекте Пекина (иначе Бейцзина, как по настоянию китайцев стало воспроизводиться на Западе название столицы Китая).'
+      },
+      ru: {
+        id: 1,
+        languageName: 'Русский',
+        audio: '../../assets/saundLanguage/china.mp3',
+        flag: '../../assets/img/defFlag.jpg',
+        titleDesc: 'Русский мяу (общепринятое название)',
+        textDesc: 'Общепринятый нормативный китайский, или общенациональный язык путунхуа, основан на диалекте Пекина (иначе Бейцзина, как по настоянию китайцев стало воспроизводиться на Западе название столицы Китая).'
+      },
+    }
+
   return (
     <>
       <HeaderContainer />
--- src/components/Question/ListQuestion.tsx
+++ src/components/Question/ListQuestion.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import styles from './question.module.scss';
 
 const ListQuestion: React.FC = () => {
+    let state = {
+        trening: [
+            { language: 'Китайский', id: 0, isSelected: true },
+            { language: 'Русский', id: 1, isSelected: false },
+            { language: 'Английский', id: 2, isSelected: false },
+            { language: 'Немецкий', id: 3, isSelected: false },
+            { language: 'Испанский', id: 4, isSelected: false },
+            { language: 'Японский', id: 5, isSelected: false }
+        ]
+    }
+    let dialogDataElement = state.trening.map(n => <li key={n.id} className={n.isSelected ? styles.list_active : ""}><span></span>{n.language}</li>);
     return (
         <ul>
-            <li className={styles.list_active}><span></span>Китайский</li>
-            <li><span></span>Русский</li>
-            <li><span></span>Английский</li>
-            <li><span></span>Немецкий</li>
-            <li><span></span>Испанский</li>
-            <li><span></span>Японский</li>
+            {dialogDataElement}
         </ul>
     )
 }
